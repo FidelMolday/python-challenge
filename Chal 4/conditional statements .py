9af11a8d15baedc0b49166e3b0b80ab040af6149
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-06-24 22:00:20 +0300

    Add files via upload

---
+++ Chal 4/conditional statements .py
@@ -0,0 +1,18 @@
+#if statements
+if 5 > 1
+print ("That's true")
+
+#nested if statements
+x = 35
+if x > 20:
+    print("Above twenty,")
+    if x < 30:
+        print("and also above 30!")
+
+#elif statements
+a = 45
+b = 45
+if b > a :
+    print("b is greater than a")
+    else a == b:
+        print("a and b are equal")            
