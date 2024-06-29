b8e2daa127ffdfe5c4ce1f9e5cb657c3f535fa20
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-06-29 23:13:32 +0300

    Add files via upload

---
+++ Chal 8/list.py
@@ -0,0 +1,5 @@
+months = ['january','february','march','april','may','june','july','august','september','october','november','december']
+print(months[0]) # print the elemment with index 0
+print(months[0:7]) # all the elements from index 0 to 6.
+months[0] = 'birthday' # exchange the value in index 0 with the word birthday
+print(months)
