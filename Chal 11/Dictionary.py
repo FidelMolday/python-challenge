1b0cd45dadfdbc291404a9b2bf83438cc5c483e4
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2024-06-29 23:17:42 +0300

    Add files via upload

---
+++ Chal 11/Dictionary.py
@@ -0,0 +1,9 @@
+music = { 'jazz': {"Coltrane": "In a Sentimental Mood",
+                          "M.Davis":"Blue in Green" ,
+                          "T.Monk":"Don't Blame Me"},
+            "classical" : {"Bach": "Cello Suit",
+                        "Mozart": "Lacrimosa",
+                        "Satie": "Gymnopédie"}}
+
+print(music["jazz"]["Coltrane"]) # we select the value of the key Coltrane
+print(music["classical"]["Mozart"])
